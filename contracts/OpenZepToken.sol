b8edd6293f7deb756d58a59996263e018d54fab2
Luis Egea <jshanks1001@gmail.com>
2020-02-18 13:15:18 -0800

    token contracts added

---
+++ contracts/OpenZepToken.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract OpenZepToken is IERC20, ERC20, ERC20Detailed {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals)
+        public
+        ERC20Detailed(_name, _symbol, _decimals)
+    {}
+}
